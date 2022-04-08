d8bd2238ca83abec31a31e663987f2c7b055d8b6
Bhavin Raichura <65218241+BhavinRaichura@users.noreply.github.com>
2022-04-09 03:10:57 +0530

    Update Merge Without Extra Space.cpp

--- array/Merge Without Extra Space.cpp
+++ array/Merge Without Extra Space.cpp
@@ -1,6 +1,27 @@
 // Merge Without Extra Space 
 
 
+//---------------------------------------------------------without using extra space-----------------------------------------------------------------------
+void merge(int arr1[], int arr2[], int n, int m){
+        int i=n-1;
+        int j=0;
+        
+        while(i>=0 && j<m){
+            if (arr1[i]<=arr2[j]){
+                j++;
+            }
+            else{
+                swap(arr1[i],arr2[j]);
+                i--;
+                j++;
+            }
+                
+        }
+        sort(arr1,arr1+n);
+        sort(arr2,arr2+m);
+}
+
+
 
 
 //------------------------------------------------merge using extra space------------------------------------------------------------------------------
