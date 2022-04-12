f5962e1a1dfd3ffc2f57582400e0f4c5bd002360
Bhavin Raichura <65218241+BhavinRaichura@users.noreply.github.com>
2022-04-13 01:45:39 +0530

    Update check whether one string is a rotation of another.cpp

--- string/check whether one string is a rotation of another.cpp
+++ string/check whether one string is a rotation of another.cpp
@@ -21,7 +21,7 @@ bool rotationalS1S2(string s1, string s2){
     return false;
 }
 
-// method 2: using string ::npos
+// method 2:------ using string ::npos 
 
 bool checkRotationalS1S2(string s1, string s2){
     if (s1.size()!=s2.size())
@@ -40,3 +40,28 @@ What is string::npos:
     As a return value, it is usually used to indicate no matches.
     */
 // https://www.geeksforgeeks.org/stringnpos-in-c-with-examples/
+
+
+// method 3: queue approach
+bool check_rotation(string s, string goal)
+{
+    if (s.size() != goal.size())
+        return false;
+    queue<char> q1;
+    for (int i = 0; i < s.size(); i++) {
+        q1.push(s[i]);
+    }
+    queue<char> q2;
+    for (int i = 0; i < goal.size(); i++) {
+        q2.push(goal[i]);
+    }
+    int k = goal.size();
+    while (k--) {
+        char ch = q2.front();
+        q2.pop();
+        q2.push(ch);
+        if (q2 == q1)
+            return true;
+    }
+    return false;
+}
