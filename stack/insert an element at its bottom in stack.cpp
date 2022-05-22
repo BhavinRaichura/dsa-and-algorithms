5aecb0bbaef679e2dc9b96478e9e5a92d6fbd08c
Bhavin Raichura <65218241+BhavinRaichura@users.noreply.github.com>
2022-05-23 02:11:18 +0530

    Update insert an element at its bottom in stack.cpp

--- stack/insert an element at its bottom in stack.cpp
+++ stack/insert an element at its bottom in stack.cpp
@@ -1,3 +1,35 @@
 // stack overflow
 // https://stackoverflow.com/questions/45130465/inserting-at-the-end-of-stack
 // Implement a method to insert an element at its bottom without using any other data structure.
+
+
+void push_to_bottom(stack<int>&st,int x){
+    if (st.empty()){
+        st.push(x);
+        return;
+    }
+    else{
+        int a= st.top();
+        st.pop();
+        push_to_bottom(st,x);
+        st.push(a);
+    }
+}
+
+int main(){
+    stack<int>st;
+  
+    st.push(2);
+    st.push(3);
+    
+    push_to_bottom(st,4);
+    push_to_bottom(st,10);
+    push_to_bottom(st,12);
+
+    while(!st.empty()){
+        cout<<st.top()<<"      ";
+        st.pop();
+    }
+
+    return 0;
+}
