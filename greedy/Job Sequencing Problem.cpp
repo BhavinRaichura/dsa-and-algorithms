7ac7ba8480985540950a1ce887e486e6d4f43b3a
Bhavin Raichura <65218241+BhavinRaichura@users.noreply.github.com>
2022-05-26 02:30:11 +0530

    Update Job Sequencing Problem.cpp

--- greedy/Job Sequencing Problem.cpp
+++ greedy/Job Sequencing Problem.cpp
@@ -8,6 +8,8 @@ error
 // agar kisi job ki deadline near hai aur us time pr koi aur job ki current deadline nahi hai to 
 // jiski deadline near hai use pahele complete kr sakta hu bhale uska profit km ho   
 
+//------------------------------------------------method 1 ---using sort according to sorting function sort(dead-line, and profit)
+
 class Solution 
 {
     public:
@@ -37,6 +39,49 @@ class Solution
             q.pop();
         }
         return {cnt, profit};
+    }
+
+};
+
+//---------------------------------------------------------------------method 2 -- normal sorting
+class Solution 
+{
+    public:
+    //Function to find the maximum profit and the number of jobs done.
+    vector<int> JobScheduling(Job arr[], int n)
+    {
+        // your code here
+        priority_queue<int,vector<int>,greater<int>>q;
+        vector<pair<int, int>> a;
+        for (int i=0;i<n;i++){
+            a.push_back({arr[i].dead,arr[i].profit});
+        }
+        
+        sort(a.begin(),a.end());
+       
+        for (int i=0;i<n;i++){
+            if(q.size()<a[i].first){
+                q.push(a[i].second);
+            }
+            else if(q.top()<a[i].second){
+                q.pop();
+                q.push(a[i].second);
+            }
+        }
+        
+        vector<int>ans(2);
+        ans[0]=0;
+        ans[1]=0;
+
+        while(!q.empty()){
+            ans[0]++;
+            ans[1]+=q.top();
+            q.pop();
+        }
+         
+
+        return ans;
+
     }
 
 };
