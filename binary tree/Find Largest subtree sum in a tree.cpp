c863fbee7acb2feafbbf432a49dd91c0fda0790c
Bhavin Raichura <65218241+BhavinRaichura@users.noreply.github.com>
2022-05-29 03:37:47 +0530

    post order traversal

--- binary tree/Find Largest subtree sum in a tree.cpp
+++ binary tree/Find Largest subtree sum in a tree.cpp
@@ -1,6 +1,27 @@
 // Find Largest subtree sum in a tree
 // post order traversal
+/*
+Given a binary tree, task is to find subtree with maximum sum in tree.
+Examples: 
+Input :       1
+            /   \
+           2      3
+          / \    / \
+         4   5  6   7
+Output : 28
+As all the tree elements are positive, the largest subtree sum is equal to sum of all tree elements.
 
+Input :       1
+            /    \
+          -2      3
+          / \    /  \
+         4   5  -6   2
+Output : 7
+Subtree with largest sum is :  -2
+                             /  \ 
+                            4    5
+Also, entire tree sum is also 7.
+*/
 class Solution {
     int maxi = INT_MIN;
 public:
