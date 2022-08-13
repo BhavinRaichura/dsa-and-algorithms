e6a35dee476cbee8d03321732a907e34a11468ad
Bhavin Raichura <65218241+BhavinRaichura@users.noreply.github.com>
2022-08-14 02:31:59 +0530

    Update Remove Nth Node From End of List.cpp

--- slow-fast-approach/Remove Nth Node From End of List.cpp
+++ slow-fast-approach/Remove Nth Node From End of List.cpp
@@ -1,10 +1,10 @@
 /*
+
+https://takeuforward.org/data-structure/remove-n-th-node-from-the-end-of-a-linked-list/
+
 Remove Nth Node From End of List
 
-
 Given the head of a linked list, remove the nth node from the end of the list and return its head.
-
- 
 
 Example 1:
     Input: head = [1,2,3,4,5], n = 2
